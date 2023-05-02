491581a746c4fb8dfc2fdb19d183cc1c5ab6e58b
hootzz <wjqwjql3@gmail.com>
2023-05-02 18:28:14 +0900

    hello world

---
+++ Test01.py
@@ -0,0 +1,20 @@
+print("hello")
+
+a = int(input("wow를 몇 개 출력하시겠습니까? : "))
+
+b = a
+c = 0
+
+for c in range(a+1) :
+    for c in range(b) :
+        print("wow", end=" ")
+    print()
+    b -= 1
+
+if a >= 50 :
+    print(a, "개나? 당신은 욕심쟁이...")
+elif a < 50 :
+    print("nice")
+
+
+
